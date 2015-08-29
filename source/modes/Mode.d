f9c05a8f7c54aea243daa7b98470a49f16bec691
Luca Morreale <luca.morreale93@gmail.com>
2015-08-29 13:09:42 +0200

    Commented Value and Mode interfaces

--- source/modes/Mode.d
+++ source/modes/Mode.d
@@ -1,12 +1,23 @@
 module modes.mode;
 
 /**
+   Interface for each modes classes providing base methods usable to interface
+   with file "/sys/class/gpio/gpioX/direction" and with memory registry.
 
+   Authors:   Luca Morreale
 
-*/
+ */
 interface Mode {
 
+    /**
+       Returns a string identifying the mode as would be read in
+       "/sys/class/gpio/gpioX/direction" file.
+     */
     string getMode();
 
+    /**
+       Returns a byte that represents the mode in the registry (without shifting);
+       as would be read in "/dev/mem"
+     */
     ubyte getBinaryMode();
 }
--- source/values/Value.d
+++ source/values/Value.d
@@ -1,12 +1,24 @@
 module values.value;
 
 /**
+   Interface for each values classes providing base methods usable to interface
+   with file "/sys/class/gpio/gpioX/value" and with memory registry.
 
-*/
+   Authors:   Luca Morreale
+
+ */
 interface Value {
 
+    /**
+       Returns an int identifying the value as would be read in
+       "/sys/class/gpio/gpioX/direction" file.
+     */
     int getValue();
 
+    /**
+       Returns a byte that represents the value in the registry (without shifting);
+       as would be read in "/dev/mem"
+     */
     ubyte getBinaryValue();
 
 }
